a1e508833ee26e59ffe3767c560484ae26caecd0
Noah <143801063+Br0a@users.noreply.github.com>
2024-08-06 11:39:25 -0300

    Add files via upload

---
+++ Js_Noah_2/codigo.js
@@ -0,0 +1,13 @@
+//declaração de variáveis
+let quantidade = 0;
+let total = 0;
+
+//Função utilizada para calcular a quantidade...
+function CalculaBurno(){
+//prompt para entrada de dados
+quantidade = parseInt(prompt("Digite a quantidade de vezes que foi preciso mandar ele calar a boca:"));
+
+total = total + quantidade;
+
+alert(total);
+}
